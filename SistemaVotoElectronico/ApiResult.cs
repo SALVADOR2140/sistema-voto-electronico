017ec22a8c3a4f157c7260911aa706dc294daaf8
TUPAK SAUL CACHIMUEL COLTA <tscachimuelc@utn.edu.ec>
2026-01-26 23:27:37 -0500

    Creacion de ApiResult para el ApiConsumer

---
+++ SistemaVotoElectronico/ApiResult.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaVotoElectronico.Modelos
+{
+    public class ApiResult<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+
+        // Métodos rápidos para responder
+        public static ApiResult<T> Ok(T data)
+        {
+            return new ApiResult<T> { Success = true, Data = data };
+        }
+
+        public static ApiResult<T> Fail(string message)
+        {
+            return new ApiResult<T> { Success = false, Message = message };
+        }
+    }
+}
